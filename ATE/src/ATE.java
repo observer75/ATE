b2cd82bf8f8c272a17a3241184fffd55b973da7f
Olexander Berdychevsky <olexander.berdychevsky@hotmail.com>
2016-04-01 16:44:29 +0300

    : Task 1 - Simple Calculator
    Task-Url:

--- ATE/src/ATE.java
+++ ATE/src/ATE.java
@@ -1,3 +1,7 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import DemoTask.GmailTest;
 
 public class ATE {
 
--- ATE/src/DemoTask/GmailTest.java
+++ ATE/src/DemoTask/GmailTest.java
@@ -1,27 +1,42 @@
 package DemoTask;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class GmailTest {
-  private WebDriver driver;
-  private String baseUrl;
+//  private WebDriver driver;
+//  private String baseUrl;
   
   @BeforeClass(alwaysRun = true)
   public void setUp() 
   {
-	  System.setProperty("webdriver.chrome.driver", "D:/Work/Projects/Jars");
-	  WebDriver driver = new ChromeDriver();
-	  baseUrl = "https://mail.google.com";
-	  driver.get(baseUrl);
+	  System.setProperty("webdriver.chrome.driver", "D:\\Work\\Projects\\Jars\\chromedriver.exe");
+	
   }
   
   @Test
   public void chromeLogin()
   {
+	  WebDriver driver = new ChromeDriver();
+	  String baseUrl = "https://mail.google.com";
 	  driver.get(baseUrl);
+	  WebElement login = driver.findElement(By.id("Email"));
+	  login.sendKeys("ivandodokas@gmail.com");
+	  
+	  WebElement next = driver.findElement(By.id("next"));
+	  next.click();
+	  
+	 
+	  
+	  WebElement passwd = driver.findElement(By.id("Passwd"));
+	  passwd.sendKeys("US!pf.avg");
+	  
+	  WebElement signIn = driver.findElement(By.id("signIn"));
+	  signIn.click();
   }
 
 }
